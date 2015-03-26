6f0c893cebefdcbcafd611e1e9d818e50dead0ea
jmeetsma <jeroen@ijsberg.nl>
2015-03-26 15:25:08 +0100

    created solution to vary included files in TeamCity plugin. Fixed metrics aggregation in Java and C#. Fixed upper level exception catch for C#.

--- boncode-teamcity-plugin-agent/src/main/java/nl/ijsberg/BoncodeTeamCityBuildProcess.java
+++ boncode-teamcity-plugin-agent/src/main/java/nl/ijsberg/BoncodeTeamCityBuildProcess.java
@@ -4,10 +4,15 @@ import jetbrains.buildServer.RunBuildException;
 import jetbrains.buildServer.agent.BuildFinishedStatus;
 import jetbrains.buildServer.agent.BuildProcess;
 import jetbrains.buildServer.agent.BuildRunnerContext;
+import nl.ijsberg.analysis.server.buildserver.AnalysisPropertiesLoader;
 import nl.ijsberg.analysis.server.buildserver.BuildServerToMonitorLink;
+import org.ijsberg.iglu.configuration.ConfigurationException;
 import org.ijsberg.iglu.logging.LogEntry;
 import org.ijsberg.iglu.logging.Logger;
+import org.ijsberg.iglu.util.properties.PropertiesSupport;
+import org.ijsberg.iglu.util.reflection.ReflectionSupport;
 import org.jetbrains.annotations.NotNull;
+import java.util.Properties;
 
 import java.util.Map;
 
@@ -23,9 +28,11 @@ public class BoncodeTeamCityBuildProcess implements BuildProcess {
     private String analysisProperties;
 	private String sourceRoot;
 	private String checkoutDir;
+	private AnalysisPropertiesLoader analysisPropertiesLoader;
+	private String alternativePropertiesInSourceRoot;
 
 	private Logger logger;
-
+	private BuildServerToMonitorLink buildServerToMonitorLink;
 
 
 	public BoncodeTeamCityBuildProcess(BuildRunnerContext context, Logger logger) {
@@ -42,25 +49,45 @@ public class BoncodeTeamCityBuildProcess implements BuildProcess {
 		monitorUploadDirectory = buildParameters.get(BuildServerToMonitorLink.MONITOR_UPLOAD_DIRECTORY);
         monitorDownloadDirectory = buildParameters.get(BuildServerToMonitorLink.MONITOR_DOWNLOAD_DIRECTORY);
 
+		String analysisPropertiesLoaderClassName = buildParameters.get(BuildServerToMonitorLink.ALTERNATIVE_PROPERTIES_LOADER);
+		if(analysisPropertiesLoaderClassName != null) {
+			try {
+				analysisPropertiesLoader = (AnalysisPropertiesLoader)ReflectionSupport.instantiateClass(analysisPropertiesLoaderClassName);
+			} catch (InstantiationException e) {
+				throw new ConfigurationException("cannot instantiate class found in property " + BuildServerToMonitorLink.ALTERNATIVE_PROPERTIES_LOADER, e);
+			}
+			alternativePropertiesInSourceRoot = buildParameters.get(BuildServerToMonitorLink.ALTERNATIVE_PROPERTIES_IN_SOURCE_ROOT);
+			BuildServerToMonitorLink.checkFile(
+					BuildServerToMonitorLink.ALTERNATIVE_PROPERTIES_IN_SOURCE_ROOT,
+					alternativePropertiesInSourceRoot,
+					false);
+		} else {
+			logger.log(new LogEntry("no value found for property " + BuildServerToMonitorLink.ALTERNATIVE_PROPERTIES_LOADER + " using standard properties loader"));
+		}
         analysisProperties = buildParameters.get(BuildServerToMonitorLink.ANALYSIS_PROPERTIES_FILENAME);
-
 		BuildServerToMonitorLink.throwIfPropertiesNotOk(
 				analysisProperties,
 				monitorUploadDirectory,
 				sourceRoot);
+		Properties properties = loadProperties();
+		buildServerToMonitorLink = new BuildServerToMonitorLink(properties, monitorUploadDirectory, monitorDownloadDirectory, logger);
+	}
+
+	private Properties loadProperties() {
+		if(analysisPropertiesLoader != null) {
+			return analysisPropertiesLoader.load(analysisProperties, alternativePropertiesInSourceRoot);
+		}
+		return PropertiesSupport.loadProperties(analysisProperties);
 	}
 
 	public void start() throws RunBuildException {
 		logger.log(new LogEntry("Starting Boncode analysis"));
-		//logger.info("Starting Boncode analysis...");
-
-        boolean result = new BuildServerToMonitorLink(analysisProperties, monitorUploadDirectory, monitorDownloadDirectory, logger).perform(sourceRoot);
+        boolean result = buildServerToMonitorLink.perform(sourceRoot);
         if(result){
             this.isFinished();
         } else {
             this.isInterrupted();
         }
-
 	}
 
 	public boolean isInterrupted() {
@@ -80,7 +107,6 @@ public class BoncodeTeamCityBuildProcess implements BuildProcess {
 	@NotNull
 	public BuildFinishedStatus waitFor() throws RunBuildException {
 		logger.log(new LogEntry("waitFor invoked"));
-
 		return BuildFinishedStatus.FINISHED_SUCCESS;
 	}
 
